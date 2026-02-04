1499a60bb7b23c670f423435aca07f77c0b09c43
Veta <vetarara@yandex.ru>
2026-02-05 02:48:56 +0300

    завершает работу над картой

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,3 +1,4 @@
+/* global ymaps */
 /* в этот файл добавляет скрипты*/
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
@@ -13,3 +14,30 @@ navToggle.addEventListener('click', () => {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+ymaps.ready(init);
+
+function init() {
+  const mapContainer = document.getElementById('map');
+
+  mapContainer.classList.remove('map--nojs');
+  mapContainer.innerHTML = '';
+
+  const map = new ymaps.Map('map', {
+    center: [59.938631, 30.323037],
+    zoom: 15
+  });
+
+  const placemark = new ymaps.Placemark(
+    [59.938631, 30.323037],
+    {},
+    {
+      iconLayout: 'default#image',
+      iconImageHref: '../images/map-pin.png',
+      iconImageSize: [113, 106],
+      iconImageOffset: [-55, -100]
+    }
+  );
+
+  map.geoObjects.add(placemark);
+}
