f73d2116fb05ce66c40443c3472d53f51ee0e974
Veta <vetarara@yandex.ru>
2026-02-05 05:54:31 +0300

    завершает слайдер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -15,6 +15,8 @@ navToggle.addEventListener('click', () => {
   }
 });
 
+// Map
+
 ymaps.ready(init);
 
 function init() {
@@ -58,3 +60,77 @@ function init() {
   });
 }
 
+// Slider
+
+const slider = document.querySelector('.slider');
+const curtain = slider.querySelector('.slider__curtain');
+const imgBefore = slider.querySelector('.slider__image--before');
+const imgAfter = slider.querySelector('.slider__image--after');
+
+let isDragging = false;
+
+function getSliderWidth() {
+  return slider.offsetWidth;
+}
+
+function updateSlider(x) {
+  const rect = slider.getBoundingClientRect();
+  let offsetX = x - rect.left;
+
+  if (offsetX < 0) {
+    offsetX = 0;
+  }
+  if (offsetX > rect.width) {
+    offsetX = rect.width;
+  }
+
+  const percent = (offsetX / rect.width) * 100;
+
+  curtain.style.left = `${percent}%`;
+
+  imgBefore.style.clipPath = `inset(0 ${100 - percent}% 0 0)`;
+  imgAfter.style.clipPath = `inset(0 0 0 ${percent}%)`;
+}
+
+curtain.addEventListener('mousedown', () => {
+  isDragging = true;
+});
+
+document.addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+document.addEventListener('mousemove', (e) => {
+  if (!isDragging) {
+    return;
+  }
+  updateSlider(e.clientX);
+});
+
+curtain.addEventListener('touchstart', () => {
+  isDragging = true;
+});
+
+document.addEventListener('touchend', () => {
+  isDragging = false;
+});
+
+document.addEventListener('touchmove', (e) => {
+  if (!isDragging) {
+    return;
+  }
+  updateSlider(e.touches[0].clientX);
+});
+
+window.addEventListener('load', () => {
+  imgBefore.style.clipPath = 'inset(0 50% 0 0)';
+  imgAfter.style.clipPath = 'inset(0 0 0 50%)';
+});
+
+
+window.addEventListener('resize', () => {
+  const beforeClip = imgBefore.style.clipPath.match(/(\d+(\.\d+)?)%/);
+  const percent = beforeClip ? parseFloat(beforeClip[1]) : 50;
+  imgBefore.style.clipPath = `inset(0 ${percent}% 0 0)`;
+  imgAfter.style.clipPath = `inset(0 0 0 ${100 - percent}%)`;
+});
