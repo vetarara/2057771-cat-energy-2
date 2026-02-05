a801162ead92d54fff548ba6e69a481f45af73b3
Veta <vetarara@yandex.ru>
2026-02-05 03:22:49 +0300

    проверка работы пина 2

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -43,7 +43,7 @@ function init() {
     {},
     {
       iconLayout: 'default#image',
-      iconImageHref: '../images/map/map-pin.png',
+      iconImageHref: 'images/map/map-pin.png',
       iconImageSize: [113, 106],
       iconImageOffset: [-55, -100]
     }
